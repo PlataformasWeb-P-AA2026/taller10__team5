21c4b603e81368015336447ef1fd97c09e8ed284
Jeank935 <jccuenca17@utpl.edu.ec>
2026-06-23 12:26:44 -0500

    admin

--- proyectociudad/ordenamiento/admin.py
+++ proyectociudad/ordenamiento/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from ordenamiento.models import Parroquia,Barrio,PresidenteBarrio
 
-# Register your models here.
+class ParroquiaAdmin(admin.ModelAdmin):
+    
+    list_display = ('nombre', 'ubicacion', 'tipo')
+    search_fields = ('nombre', 'ubicacion')
+
+admin.site.register(Parroquia, ParroquiaAdmin)
+
+
+class BarrioaAdmin(admin.ModelAdmin):
+    
+    list_display = ('nombre', 'numero_viviendas', 'numero_parques','numero_edificios_residenciales','parroquia')
+    search_fields = ('nombre', 'parroquia')
+
+admin.site.register(Barrio, BarrioaAdmin)
+
+class PresidenteBarrioAdmin(admin.ModelAdmin):
+    
+    list_display = ('cedula', 'nickname', 'edad','profesion','barrio')
+    search_fields = ('nombre', 'nickname')
+ 
+admin.site.register(PresidenteBarrio,PresidenteBarrioAdmin)
