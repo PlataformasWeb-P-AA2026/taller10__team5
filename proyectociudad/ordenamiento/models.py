8c4a464fb41fe5b132591d7485a2614eb4f5d66f
EmilioOne05 <emiliojosepe05@gmail.com>
2026-06-23 20:12:42 -0500

    Final3

--- proyectociudad/ordenamiento/models.py
+++ proyectociudad/ordenamiento/models.py
@@ -21,9 +21,11 @@ class Parroquia(models.Model):
     def __str__(self):
         return f"{self.nombre} - Ubicacion:{self.ubicacion} - Tipo:{self.tipo}"
 
-    @property
     def numero_parques_total(self):
-        return sum(b.numero_parques for b in self.barrios.all())
+        total = 0
+        for b in self.barrios.all():
+            total = total + b.numero_parques
+        return total
 
 
 
